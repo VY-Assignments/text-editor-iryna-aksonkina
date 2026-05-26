d5170aa430e4cf02f843cffc6fc4dbdb213f25a5
User <aksenkina.irra1@gmail.com>
2026-05-26 21:55:37 +0300

    Створення рядків, робота з пам'яттю та виведення тексту

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+typedef struct {
+    char** text;
+    int* line_lengths;
+    int line_count;
+} TextEditor;
 
-    printf("Привіт, світе!\n");
+void init_editor(TextEditor* editor) {
+    editor->text = NULL;
+    editor->line_lengths = NULL;
+    editor->line_count = 0;
+}
 
-    return 0;
+void start_new_line(TextEditor* editor) {
+    int new_count = editor->line_count + 1;
+    editor->text = realloc(editor->text, new_count * sizeof(char*));
+    editor->line_lengths = realloc(editor->line_lengths, new_count * sizeof(int));
+
+    editor->text[editor->line_count][0] = '/0';
+    editor->line_lengths[editor->line_count] = 0;
+    editor->line_count = new_count;
+
+}
+
+void print_text(TextEditor* editor) {
+    for (int i = 0; i < editor->line_count; i++) {
+        printf("%s\n", editor->text[i]);
+    }
 }
